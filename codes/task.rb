607180f67c457bbf7d126604dc21d3c3ff495017
newmankaleem <kaleemsyed1289gmail.com>
2018-12-12 10:45:55 +0530

    submit all pending changes

--- codes/task.rb
+++ codes/task.rb
@@ -4,11 +4,11 @@ directory 'sha345' do
  # mode '0755'
  action :create
 
-end
 
-file 'taskfile' do
+
+file 'sha345/taskfile' do
   content "first file"	
-  content '<html>This is a placeholder for the home page.</html>'
+  content '<html>This is a placeholder for the home page.</html>!\n'
   #mode '0755'
   #owner 'web_admin'
   #group 'web_admin'
@@ -17,7 +17,43 @@ end
 
 #sleep 30
 
-#file 'taskfile' do
- # action :delete
+#file 'sha345/taskfile' do
+ #action :delete
 
 #end
+
+
+file 'sha345/dddd.txt' do
+  content "second file"
+  content '<html>This is a placeholder for the home page.</html>!\n'
+  action :create
+
+end
+
+
+file 'sha345/rrr.txt' do
+  content "third file"
+  content '<html>This is a placeholder for the home page.</html>!\n'
+  action :create
+
+end
+
+file 'sha345/sdsd.txt' do
+  content "fourth file"
+  content '<html>This is a placeholder for the home page.</html>!\n'
+  action :create
+
+end
+
+
+
+
+[ 'taskfile', 'dddd.txt' , 'rrr.txt' , 'sdsd.txt' ].each do |basha|
+   directory "#{sha345}/#{basha}" do
+    #contents "#{sha345} is deleted files!\n"
+    action :delete
+    end
+end
+
+end
+
